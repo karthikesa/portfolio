d18562d07871e90f07e9a6ee2b06b2faa75a08b0
karthikEswaran05 <karthik.eswaran@hashagile.com>
2025-03-13 23:27:08 +0530

    first commit

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -69,21 +69,18 @@ const About = () => {
                 <div className="progress">
                   <span className="skill"><span>HTML</span> <i className="val">100%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
 
                 <div className="progress">
                   <span className="skill"><span>CSS</span> <i className="val">90%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
 
                 <div className="progress">
                   <span className="skill"><span>JavaScript</span> <i className="val">75%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
 
@@ -94,21 +91,18 @@ const About = () => {
                 <div className="progress">
                   <span className="skill"><span>PHP</span> <i className="val">80%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
 
                 <div className="progress">
                   <span className="skill"><span>WordPress/CMS</span> <i className="val">90%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
 
                 <div className="progress">
                   <span className="skill"><span>Photoshop</span> <i className="val">55%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
 
